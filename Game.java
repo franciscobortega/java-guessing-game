869419d6f4d941d7c97842c75c84d4e78eaa7556
Bryan Ortega <fbryanortega@gmail.com>
2023-10-24 20:34:44 -0500

    Finished guessing game functionality

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /** A number guessing game. */
 public class Game {
@@ -8,9 +9,37 @@ public class Game {
     Scanner input = new Scanner(System.in);
 
     // Get user's name
-    System.out.println("Enter your name:");
+    System.out.print("Enter your name: ");
     String name = input.nextLine();
     // Test that everything works
     System.out.println(name);
+    
+    // Create an instance of the Random object
+    Random rand = new Random();
+    
+    // Call method nextInt and pass in upper range (exclusive)
+    int number = rand.nextInt(101);
+    System.out.println(number);
+
+    int numberOfGuesses = 0;
+
+    boolean guessed = false;
+
+    while (!guessed) {
+      System.out.print("Enter your guess: ");
+      int guess = input.nextInt();
+      numberOfGuesses++;
+
+      if (guess == number) {
+        System.out.print("You won! Your total guesses: ");
+        System.out.println(numberOfGuesses);
+        break;
+      } if (guess < number) {
+        System.out.println("Too low!");
+      } else if (guess > number) {
+        System.out.println("Too high!");
+      }
+
+    }
   }
 }
