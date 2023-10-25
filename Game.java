b1e37d92f669c84cdf4d3ba43d9e546a5876ebd6
Bryan Ortega <fbryanortega@gmail.com>
2023-10-24 20:11:09 -0500

    Added scanner class

--- Game.java
+++ Game.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 /** A number guessing game. */
 public class Game {
   public static void main(String[] args) {
     System.out.println("Welcome to the Guessing Game!");
+
+    Scanner input = new Scanner(System.in);
+
+    // Get user's name
+    System.out.println("Enter your name:");
+    String name = input.nextLine();
+    // Test that everything works
+    System.out.println(name);
   }
 }
